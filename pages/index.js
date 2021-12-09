7dd7db90e812e61f2025e0764e93afa1949ec561
unknown <jayJakskie2727siguenza@gmail.com>
2021-12-09 15:11:34 +0800

    index update

--- pages/index.js
+++ pages/index.js
@@ -75,32 +75,6 @@ export default function Home({
 			/>
 		</Layout>
 	);
-	// return (
-	// 	<>
-	// 		<Head>
-	// 			<title>Tech Blog</title>
-	// 			<meta
-	// 				name="description"
-	// 				content="Keep up to date with the latest trends in tech"
-	// 			/>
-	// 			<link rel="icon" href="/favicon.ico" />
-	// 			{/* You can add more metadata here, like open graph tags for social media, etc */}
-	// 		</Head>
-
-	// 		<div className="container pt-5">
-	// 			<h1 className="text-center pb-5">Tech Blog</h1>
-	// 			<div className="row">
-	// 				<div className="col-lg-8">
-	// 					<h2 className="pb-3">Our blog posts</h2>
-	// 				</div>
-	// 				<div className="col-lg-4">
-	// 					<h2 className="pb-3">Events</h2>
-	// 					{/* {jsxEvents} */}
-	// 				</div>
-	// 			</div>
-	// 		</div>
-	// 	</>
-	// );
 }
 
 export async function getStaticProps() {
